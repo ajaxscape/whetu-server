01d18a16841ca53b42128d9ed2aa8a888d32a78e
Ben on windows <ben@ajaxscape.com>
2018-02-19 08:56:29 +0000

    Work on webpack

--- src/server/app.js
+++ src/server/app.js
@@ -4,11 +4,10 @@ const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-const sassMiddleware = require('node-sass-middleware')
 
 const index = require('./routes/index')
-const single = require('./routes/single-player')
-const multi = require('./routes/multi-player')
+const single = require('./routes/single')
+const multi = require('./routes/multi')
 
 const app = express()
 
@@ -22,13 +21,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(process.cwd(), 'public')))
 
 app.use('/', index)
 app.use('/single', single)
